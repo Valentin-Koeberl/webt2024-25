cacd4771e348f869c686fa26b3940615a198608e
IHateSEW <168427476+IHateSEW@users.noreply.github.com>
2024-11-13 15:54:57 +0100

    addSong gelöscht. Fortschritt für user story 2, jedoch nicht fertig

--- Server Responses with PHP in JSON format/index.php
+++ Server Responses with PHP in JSON format/index.php
@@ -1,30 +1,10 @@
 <?php
 
 require_once 'ost.php';
+require_once 'seeder.php';
 
-// Beispiel-Daten
-$ost1 = new OST(1, "The Legend of Zelda: Ocarina of Time OST", "The Legend of Zelda: Ocarina of Time", 1998);
-$ost1->addSong(new Song(1, "Main Theme", "Koji Kondo", 1, 120));
-$ost1->addSong(new Song(2, "Zelda's Lullaby", "Koji Kondo", 2, 90));
+$data = new Seeder();
 
-$ost2 = new OST(2, "Final Fantasy VII OST", "Final Fantasy VII", 1997);
-$ost2->addSong(new Song(1, "Opening - Bombing Mission", "Nobuo Uematsu", 1, 162));
-$ost2->addSong(new Song(2, "Aerith's Theme", "Nobuo Uematsu", 2, 240));
-
-$ost_list = [$ost1, $ost2];
-
-// GET-Parameter abrufen
-$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-
-// JSON-Antwort generieren
-header('Content-Type: application/json');
-
-foreach ($ost_list as $ost) {
-    if ($ost->toArray()['id'] == $id) {
-        echo json_encode($ost->toArray());
-        exit;
-    }
+foreach ($data as $ost) {
+    echo $ost;
 }
-
-echo json_encode(['error' => 'OST not found']);
-?>
--- Server Responses with PHP in JSON format/seeder.php
+++ Server Responses with PHP in JSON format/seeder.php
@@ -7,24 +7,24 @@ class Seeder{
         return array(
             new OST(1, "The Legend of Zelda: Ocarina of Time OST", "The Legend of Zelda: Ocarina of Time", 1998,
                 array(
-                    addSong(new Song(1, "Rainbow Road", "Koji Kondo", 1, 400)),
-                    addSong(new Song(2, "Zelda's Lullaby", "Koji Kondo", 2, 90)),
-                    addSong(new Song(3, "Link Disstrack", "Koji Kondo", 3, 50)),
-                    addSong(new Song(4, "Zelda's Lullaby", "Koji Kondo", 4, 90))
+                    new Song(1, "Rainbow Road", "Koji Kondo", 1, 400),
+                    new Song(2, "Zelda's Lullaby", "Koji Kondo", 2, 90),
+                    new Song(3, "Link Disstrack", "Koji Kondo", 3, 50),
+                    new Song(4, "Zelda's Lullaby", "Koji Kondo", 4, 90)
                 )),
             new OST(2, "Final Fantasy VII OST", "Final Fantasy VII", 1997,
                 array(
-                    addSong(new Song(1, "Opening - Bombing Mission", "Nobuo Uematsu", 1, 162)),
-                    addSong(new Song(2, "Aerith's Theme", "Nobuo Uematsu", 2, 240)),
-                    addSong(new Song(3, "Aerial Flip", "Nobuo Uematsu", 3, 60)),
-                    addSong(new Song(4, "End Boss", "Nobuo Uematsu", 4, 140))
+                    new Song(1, "Opening - Bombing Mission", "Nobuo Uematsu", 1, 162),
+                    new Song(2, "Aerith's Theme", "Nobuo Uematsu", 2, 240),
+                    new Song(3, "Aerial Flip", "Nobuo Uematsu", 3, 60),
+                    new Song(4, "End Boss", "Nobuo Uematsu", 4, 140)
                 )),
             new OST(3, "Mario Kart 8 OST", "Mario Kart", 2017,
                 array(
-                    addSong(new Song(1, "Rainbow Road", "Koji Kondo", 1, 400)),
-                    addSong(new Song(2, "Kuh-Muh Weide", "Koji Kondo", 2, 100)),
-                    addSong(new Song(3, "Toads Hafenstadt", "Koji Kondo", 3, 90)),
-                    addSong(new Song(4, "Marios Piste", "Koji Kondo", 4, 200))
+                    new Song(1, "Rainbow Road", "Koji Kondo", 1, 400),
+                    new Song(2, "Kuh-Muh Weide", "Koji Kondo", 2, 100),
+                    new Song(3, "Toads Hafenstadt", "Koji Kondo", 3, 90),
+                    new Song(4, "Marios Piste", "Koji Kondo", 4, 200)
                 ))
         );
     }
