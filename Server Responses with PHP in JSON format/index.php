a9c3be23f5f2b3e59ec71f27a1b4cde0ca257237
Valentin Köberl <valentin.koeberl@htl.rennweg.at>
2024-12-04 14:34:11 +0100

    Updated Version
    All user stories work now in a correct way

--- Server Responses with PHP in JSON format/index.php
+++ Server Responses with PHP in JSON format/index.php
@@ -1,8 +1,27 @@
 <?php
+include_once 'seeder.php';
 
-require_once 'ost.php';
-require_once 'seeder.php';
+// OST-Daten laden
+$ost_list = Seeder::getData();
 
-$data = Seeder::getData();
+// Abfrageparameter
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-echo json_encode($data, JSON_PRETTY_PRINT);
+header('Content-Type: application/json');
+
+if ($id === 0) {
+    // Alle OSTs zurückgeben, wenn keine bestimmte ID angefordert wird
+    $all_osts = array_map(function($ost) {
+        return $ost->jsonSerialize();
+    }, $ost_list);
+    echo json_encode($all_osts);
+} else {
+    // Spezifisches OST zurückgeben, wenn eine ID angegeben wird
+    foreach ($ost_list as $ost) {
+        if ($ost->jsonSerialize()['id'] == $id) {
+            echo json_encode($ost->jsonSerialize());
+            exit;
+        }
+    }
+    echo json_encode(['error' => 'OST nicht gefunden']);
+}
